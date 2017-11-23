e57125602e77a98ef62a823c2482c31e43ffe9dc
Ciao Huang <riverciao@gmail.com>
2017-11-23 17:59:50 +0800

    can't use tableview

---
+++ One-image Viewer/Photo.swift
@@ -0,0 +1,9 @@
+//
+//  Photo.swift
+//  One-image Viewer
+//
+//  Created by riverciao on 2017/11/23.
+//  Copyright © 2017年 riverciao. All rights reserved.
+//
+
+import Foundation
---
+++ One-image Viewer/SelectPhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectPhotoTableViewCell.swift
+//  One-image Viewer
+//
+//  Created by riverciao on 2017/11/23.
+//  Copyright © 2017年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class SelectPhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
