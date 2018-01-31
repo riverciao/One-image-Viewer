c1b564c63e9e6cc4b913f3360c3dd9ebe04efcac
Ciao Huang <riverciao@gmail.com>
2018-02-01 01:36:11 +0800

    refactor

--- One-image Viewer/ZoomInViewController.swift
+++ One-image Viewer/ZoomInViewController.swift
@@ -147,22 +147,22 @@ class ZoomInViewController: UIViewController, UIScrollViewDelegate,
 
     // MARK: Method
 
-        // 讓圖片置中, 每次縮放之後會被呼叫
-        func scrollViewDidZoom(_ scrollView: UIScrollView) {
-            let imageViewSize = imageView.frame.size
-            let scrollViewSize = scrollView.bounds.size
+    // 讓圖片置中, 每次縮放之後會被呼叫
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
 
-            let verticalPadding = imageViewSize.height < scrollViewSize.height ?
-                (scrollViewSize.height - imageViewSize.height) / 2 : 0
-            let horizontalPadding = imageViewSize.width < scrollViewSize.width ?
-                (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageViewSize.width) / 2 : 0
 
-            scrollView.contentInset = UIEdgeInsets(
-                top: verticalPadding,
-                left: horizontalPadding,
-                bottom: verticalPadding,
-                right: horizontalPadding)
-        }
+        scrollView.contentInset = UIEdgeInsets(
+            top: verticalPadding,
+            left: horizontalPadding,
+            bottom: verticalPadding,
+            right: horizontalPadding)
+    }
 
     @objc func goToPickAnImage(sender: UIButton) {
         openGallary()
